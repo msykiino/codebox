95870a41216bd222578b4595b514a10f85c3095a
msykiino <msyk.iino@gmail.com>
2014-04-24 17:36:43 +0900

    updated path to save scripts.

--- roles/casual-do/files/root/bin/buildfirewall.sh
+++ roles/casual-do/files/root/bin/buildfirewall.sh
@@ -91,7 +91,7 @@ $iptables -A INPUT -p tcp --dport  22 -j ACCEPT
 
 # http, https
 $iptables -A INPUT -p tcp --dport  80 -j ACCEPT
-$iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+#$iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 
 # ftp
 #$iptables -A INPUT -p tcp --sport  20 -j ACCEPT
